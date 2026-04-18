c9a14fc8485e7fbcf3e8b2ce7c1c04dd94247116
nicoabraham46 <nicoabraham001@gmail.com>
2026-04-18 18:38:12 -0300

    fix pokemon lottie con mounted guard

--- components/PokemonRunner.tsx
+++ components/PokemonRunner.tsx
@@ -1,11 +1,32 @@
 "use client"
+import { DotLottieReact } from '@lottiefiles/dotlottie-react'
+import { useState, useEffect } from 'react'
+
+const pokemons = [
+  '/Pikachu.lottie',
+  '/Bulbasaur.lottie',
+]
 
 export function PokemonRunner() {
+  const [current, setCurrent] = useState(0)
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => { setMounted(true) }, [])
+
+  if (!mounted) return null
+
   return (
-    <div className="pokemon-runner">
-      <img
-        src="https://media.giphy.com/media/WsNbxuFkLi3IuGI9uI/giphy.gif"
-        alt="pokemon"
+    <div
+      className="pokemon-runner"
+      onAnimationIteration={() =>
+        setCurrent((prev) => (prev + 1) % pokemons.length)
+      }
+    >
+      <DotLottieReact
+        key={current}
+        src={pokemons[current]}
+        loop
+        autoplay
         style={{ height: 50, width: 'auto' }}
       />
     </div>
