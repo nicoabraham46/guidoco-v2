6758dab279082dd65053ddf2eca2884cfc0751a6
nicoabraham46 <nicoabraham001@gmail.com>
2026-04-18 18:36:49 -0300

    test gif fallback pokemon navbar

--- components/PokemonRunner.tsx
+++ components/PokemonRunner.tsx
@@ -1,27 +1,11 @@
 "use client"
-import { DotLottieReact } from '@lottiefiles/dotlottie-react'
-import { useState } from 'react'
-
-const pokemons = [
-  '/Pikachu.lottie',
-  '/Bulbasaur.lottie',
-]
 
 export function PokemonRunner() {
-  const [current, setCurrent] = useState(0)
-
   return (
-    <div
-      className="pokemon-runner"
-      onAnimationIteration={() =>
-        setCurrent((prev) => (prev + 1) % pokemons.length)
-      }
-    >
-      <DotLottieReact
-        key={current}
-        src={pokemons[current]}
-        loop
-        autoplay
+    <div className="pokemon-runner">
+      <img
+        src="https://media.giphy.com/media/WsNbxuFkLi3IuGI9uI/giphy.gif"
+        alt="pokemon"
         style={{ height: 50, width: 'auto' }}
       />
     </div>
