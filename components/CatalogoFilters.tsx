6aebecc487e66b86e6e16300d0c96e332c0deb3e
nicoabraham46 <nicoabraham001@gmail.com>
2026-04-27 16:46:05 -0300

    fix: filtro de año ampliado desde 1996 hasta 2026

--- components/CatalogoFilters.tsx
+++ components/CatalogoFilters.tsx
@@ -146,13 +146,9 @@ export default function CatalogoFilters({ category, q, sort, total, pokemonType,
             style={{ height: 34 }}
           >
             <option value="">Todos los años</option>
-            <option value="2026">2026</option>
-            <option value="2025">2025</option>
-            <option value="2024">2024</option>
-            <option value="2023">2023</option>
-            <option value="2022">2022</option>
-            <option value="2021">2021</option>
-            <option value="2020">2020</option>
+            {Array.from({ length: 2026 - 1996 + 1 }, (_, i) => 2026 - i).map((y) => (
+              <option key={y} value={String(y)}>{y}</option>
+            ))}
           </select>
         )}
 
